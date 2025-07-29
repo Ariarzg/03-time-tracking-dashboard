21c3b90c580f95b951265d00648cb45b160d2247
Aria Rouzegar <ariarzg@gmail.com>
2025-07-29 22:01:12 +0330

    Add Categories Via Javascript And Add Interactivity to Time Periods

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { loadCategories, categories } from './data.js';
 
 renderLayout();
 
-let timePeriod = 'weekly';
+let timePeriod = 'monthly';
 
 async function renderLayout() {
   await loadCategories();
@@ -14,6 +14,19 @@ async function renderLayout() {
   let pageHTML = renderHeader() + renderCategories();
 
   document.getElementById('app').innerHTML = pageHTML;
+
+  document.getElementById('daily').addEventListener('click', () => {
+    timePeriod = 'daily';
+    renderLayout();
+  });
+  document.getElementById('weekly').addEventListener('click', () => {
+    timePeriod = 'weekly';
+    renderLayout();
+  });
+  document.getElementById('monthly').addEventListener('click', () => {
+    timePeriod = 'monthly';
+    renderLayout();
+  });
 }
 
 function renderCategories() {
@@ -21,7 +34,38 @@ function renderCategories() {
   categories.forEach((category) => {
     console.log(category);
 
-    let titleFormatted = category.title.toLowerCase().replace(' ', '-');
+    const title = category.title;
+    let titleFormatted = title.toLowerCase().replace(' ', '-');
+
+    // console.log(category.timeframes.daily.current);
+    const timeframes = category.timeframes;
+
+    const currentHours =
+      timePeriod === 'daily'
+        ? timeframes.daily.current
+        : timePeriod === 'weekly'
+        ? timeframes.weekly.current
+        : timePeriod === 'monthly'
+        ? timeframes.monthly.current
+        : 0;
+
+    const previousHours =
+      timePeriod === 'daily'
+        ? timeframes.daily.previous
+        : timePeriod === 'weekly'
+        ? timeframes.weekly.previous
+        : timePeriod === 'monthly'
+        ? timeframes.monthly.previous
+        : 0;
+
+    const timePeriodFormatted =
+      timePeriod === 'daily'
+        ? 'Day'
+        : timePeriod === 'weekly'
+        ? 'Week'
+        : timePeriod === 'monthly'
+        ? 'Month'
+        : '';
 
     html += `
       <section class="bg-${titleFormatted} rounded-2xl overflow-hidden relative">
@@ -33,15 +77,15 @@ function renderCategories() {
         <div
           class="p-6 mt-10 bg-navy-900 rounded-2xl grid grid-cols-2 justify-between items-center gap-2 relative z-20"
         >
-          <h3 class="text-titles font-medium">Work</h3>
+          <h3 class="text-titles font-medium">${title}</h3>
           <img
             src="/images/icon-ellipsis.svg"
             alt="Ellipsis Icon"
             class="ml-auto"
           />
-          <p class="text-3xl font-light">32hrs</p>
+          <p class="text-fluid-hours font-light">${currentHours}hrs</p>
 
-          <p class="text-navy-200">Last Week - 36hrs</p>
+          <p class="text-navy-200 font-light text-fluid-last text-end">Last ${timePeriodFormatted} - ${previousHours}hrs</p>
         </div>
       </section>
     `;
@@ -60,14 +104,20 @@ function renderHeader() {
             class="w-16 border-3 border-white rounded-full"
           />
           <div class="">
-            <p class="text-navy-200 text-xs">Report for</p>
-            <h2 class="text-white text-titles">Jeremy Robson</h2>
+            <p class="text-navy-200 text-sm">Report for</p>
+            <h2 class="text-white text-fluid-name font-light">Jeremy Robson</h2>
           </div>
         </div>
       </div>
-      <a class="inline text-center p-4 text-navy-500">Daily</a>
-      <a class="inline text-center p-4">Weekly</a>
-      <a class="inline text-center p-4 text-navy-500">Monthly</a>
+      <a class="inline text-fluid-paragraph text-center p-4 ${
+        timePeriod === 'daily' ? '' : 'text-navy-500'
+      } cursor-pointer hover:text-white" id="daily">Daily</a>
+      <a class="inline text-fluid-paragraph text-center p-4 ${
+        timePeriod === 'weekly' ? '' : 'text-navy-500'
+      } cursor-pointer  hover:text-white" id="weekly">Weekly</a>
+      <a class="inline text-fluid-paragraph text-center p-4 ${
+        timePeriod === 'monthly' ? '' : 'text-navy-500'
+      } cursor-pointer  hover:text-white" id="monthly">Monthly</a>
     </section>
   `;
 }
